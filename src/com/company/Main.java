05ae0de6260b141e9a5e2dd02a4282475b9f22c4
Admin <kr77.kot@gmail.com>
2021-08-23 12:05:09 +0300

    доделать_в_свободное_время

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,7 +10,7 @@ public class Main {
         JFrame window = new JFrame("TikTakToe");                   //наше главное окно
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //добавим кнопку Х, закрывающую окно
         window.setSize(400, 400);                          //размер окна
-        window.setLayout(new BorderLayout());                         //менеджер компановки
+        window.setLayout(new BorderLayout());                         //менеджер компоновки
         window.setLocationRelativeTo(null);                          //чтобы окно было по центру экрана
         window.setVisible(true);                                    //включаем видимость окна
         TicTacToe game = new TicTacToe();
