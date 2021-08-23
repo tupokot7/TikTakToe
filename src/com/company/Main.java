542aa629f0cd42eb96a4b8de7cf0644133b374d1
Admin <kr77.kot@gmail.com>
2021-08-23 12:02:54 +0300

    доделать_в_свободное_время

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,15 +6,15 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Запускаем игру...");
-        JFrame window = new JFrame("TikTakToe"); //наше главное окно
+        System.out.println("Запускаем игру...");                       //Это тупо для консоли
+        JFrame window = new JFrame("TikTakToe");                   //наше главное окно
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //добавим кнопку Х, закрывающую окно
-        window.setSize(400, 400); //размер окна
-        window.setLayout(new BorderLayout()); //менеджер компановки
-        window.setLocationRelativeTo(null); //чтобы окно было по центру экрана
-        window.setVisible(true); //включаем видимость окна
+        window.setSize(400, 400);                          //размер окна
+        window.setLayout(new BorderLayout());                         //менеджер компановки
+        window.setLocationRelativeTo(null);                          //чтобы окно было по центру экрана
+        window.setVisible(true);                                    //включаем видимость окна
         TicTacToe game = new TicTacToe();
         window.add(game);
-        System.out.println("Конец...");
+        System.out.println("Конец...");                          //Это тоже тупо для консоли
     }
 }
